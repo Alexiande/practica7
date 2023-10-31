cb39b9661392b2bb0959dbaed7f2a55e21f04af6
ALEKS <ansalekseeva1@gmail.com>
2023-10-31 16:50:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/practica7/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.practica7.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD81B1B)
+val PurpleGrey80 = Color(0xFFDCC2C7)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFFA45061)
+val PurpleGrey40 = Color(0xFF715B60)
+val Pink40 = Color(0xFF7D5257)
---
+++ app/src/main/java/com/example/practica7/ui/theme/Type.kt
@@ -0,0 +1,32 @@
+package com.example.practica7.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 26.sp,
+        lineHeight = 34.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+        lineHeight = 38.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 21.sp,
+        lineHeight = 26.sp,
+        letterSpacing = 0.5.sp
+    )
+)
